3644c48db2355bd57bfead652df3c0a1a0afeef6
mrrikohermansyah <mr.rikohermansyah@gmail.com>
2025-11-06 11:02:26 +0700

    add fallback firebase config

--- assets/js/utils/firebase-config.js
+++ assets/js/utils/firebase-config.js
@@ -1,5 +1,13 @@
 // Firebase configuration dari CONFIG
-const firebaseConfig = window.CONFIG.FIREBASE_CONFIG;
+const firebaseConfig = window.CONFIG?.FIREBASE_CONFIG || {
+    apiKey: "AIzaSyCQR--hn0RDvDduCjA2Opa9HLzyYn_GFIs",
+    authDomain: "itticketing-f926e.firebaseapp.com",
+    projectId: "itticketing-f926e",
+    storageBucket: "itticketing-f926e.firebasestorage.app",
+    messagingSenderId: "10687213121",
+    appId: "1:10687213121:web:af3b530a7c45d3ca2d8a7e",
+    measurementId: "G-8H0EP72PC2"
+};
 
 // Initialize Firebase
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
