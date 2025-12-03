fdcb6e252a0033179a77040497292c2375f2985a
mrrikohermansyah <mr.rikohermansyah@gmail.com>
2025-12-03 13:25:25 +0700

    fixing mobile device create damin first PIN sha256Fallback is not defined

--- pages/admin/assets/js/create-first-admin.js
+++ pages/admin/assets/js/create-first-admin.js
@@ -296,73 +296,77 @@ class FirstAdminSetup {
   }
 
   sha256Fallback(ascii) {
-    function rightRotate(value, amount) {
-      return (value >>> amount) | (value << (32 - amount));
+    function rotr(n, x) {
+      return (x >>> n) | (x << (32 - n));
     }
-    var mathPow = Math.pow,
-      maxWord = mathPow(2, 32);
-    var lengthProperty = "length";
+    function toUtf8(str) {
+      return unescape(encodeURIComponent(str));
+    }
+    var maxWord = Math.pow(2, 32);
     var i, j;
     var result = "";
     var words = [];
-    var asciiBitLength = ascii[lengthProperty] * 8;
-    var hash = [];
-    var k = [];
-    var primeCounter = 0;
-    var isComposite = {};
-    for (var candidate = 2; primeCounter < 64; candidate++) {
+    var msg = toUtf8(ascii);
+    var msgBitLen = msg.length * 8;
+    var H = [],
+      K = [],
+      isComposite = {};
+    for (var candidate = 2; K.length < 64; candidate++) {
       if (!isComposite[candidate]) {
-        for (i = 0; i < 313; i += candidate) {
-          isComposite[i] = candidate;
-        }
-        hash[primeCounter] = (mathPow(candidate, 0.5) * maxWord) | 0;
-        k[primeCounter++] = (mathPow(candidate, 1 / 3) * maxWord) | 0;
+        for (i = candidate * candidate; i <= 313; i += candidate)
+          isComposite[i] = true;
+        H.push((Math.pow(candidate, 0.5) * maxWord) | 0);
+        K.push((Math.pow(candidate, 1 / 3) * maxWord) | 0);
       }
     }
-    ascii += "\x80";
-    while ((ascii[lengthProperty] % 64) - 56) ascii += "\x00";
-    for (i = 0; i < ascii[lengthProperty]; i++) {
-      j = ascii.charCodeAt(i);
-      words[i >> 2] |= j << (((3 - i) % 4) * 8);
+    for (i = 0; i < msg.length; i++) {
+      words[i >> 2] |= msg.charCodeAt(i) << (((3 - i) % 4) * 8);
     }
-    words[words[lengthProperty]] = (asciiBitLength / maxWord) | 0;
-    words[words[lengthProperty]] = asciiBitLength;
-    for (j = 0; j < words[lengthProperty]; ) {
-      var w = words.slice(j, (j += 16));
-      var oldHash = hash;
-      hash = hash.slice(0, 8);
+    words[words.length] = (msgBitLen / maxWord) | 0;
+    words[words.length] = msgBitLen;
+    var w = new Array(64);
+    var hash = H.slice(0);
+    for (j = 0; j < words.length; ) {
+      var block = words.slice(j, (j += 16));
       for (i = 0; i < 64; i++) {
         var w15 = w[i - 15],
           w2 = w[i - 2];
+        w[i] =
+          i < 16
+            ? block[i]
+            : (w[i - 16] +
+                (rotr(7, w15) ^ rotr(18, w15) ^ (w15 >>> 3)) +
+                w[i - 7] +
+                (rotr(17, w2) ^ rotr(19, w2) ^ (w2 >>> 10))) |
+              0;
         var a = hash[0],
-          e = hash[4];
-        var temp1 =
-          (hash[7] +
-            (rightRotate(e, 6) ^ rightRotate(e, 11) ^ rightRotate(e, 25)) +
-            ((e & hash[5]) ^ (~e & hash[6])) +
-            k[i] +
-            (w[i] =
-              i < 16
-                ? w[i]
-                : w[i - 16] +
-                  (rightRotate(w15, 7) ^ rightRotate(w15, 18) ^ (w15 >>> 3)) +
-                  w[i - 7] +
-                  (rightRotate(w2, 17) ^ rightRotate(w2, 19) ^ (w2 >>> 10)))) |
+          b = hash[1],
+          c = hash[2],
+          d = hash[3],
+          e = hash[4],
+          f = hash[5],
+          g = hash[6],
+          h7 = hash[7];
+        var t1 =
+          (h7 +
+            (rotr(6, e) ^ rotr(11, e) ^ rotr(25, e)) +
+            ((e & f) ^ (~e & g)) +
+            K[i] +
+            w[i]) |
           0;
-        var temp2 =
-          (rightRotate(a, 2) ^ rightRotate(a, 13) ^ rightRotate(a, 22)) +
-          ((a & hash[1]) ^ (a & hash[2]) ^ (a & hash[1] & hash[2]));
-        hash = [(temp1 + temp2) | 0].concat(hash);
-        hash[4] = (hash[4] + temp1) | 0;
+        var t2 =
+          ((rotr(2, a) ^ rotr(13, a) ^ rotr(22, a)) +
+            ((a & b) ^ (a & c) ^ (b & c))) |
+          0;
+        hash = [(t1 + t2) | 0, a, b, c, (d + t1) | 0, e, f, g];
       }
-      for (i = 0; i < 8; i++) {
-        hash[i] = (hash[i] + oldHash[i]) | 0;
-      }
+      for (i = 0; i < 8; i++) H[i] = (H[i] + hash[i]) | 0;
+      hash = H.slice(0);
     }
     for (i = 0; i < 8; i++) {
       for (j = 3; j + 1; j--) {
-        var b = (hash[i] >> (j * 8)) & 255;
-        result += (b < 16 ? 0 : "") + b.toString(16);
+        var b = (H[i] >> (j * 8)) & 255;
+        result += (b < 16 ? "0" : "") + b.toString(16);
       }
     }
     return result;
